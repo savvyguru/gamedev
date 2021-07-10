978763ed8ddccb2dffe347b6e2eec01d0179034f
savvyguru <pungtuckwei@gmail.com>
2021-07-11 01:30:54 +0800

    start button

---
+++ Assets/MenuController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+     void Awake()
+    {
+        Time.timeScale = 0.0f;
+    }
+    public void StartButtonClicked()
+  {
+      foreach (Transform eachChild in transform)
+      {
+          if (eachChild.name != "Score")
+          {
+              Debug.Log("Child found. Name: " + eachChild.name);
+              // disable them
+              eachChild.gameObject.SetActive(false);
+              Time.timeScale = 1.0f;
+          }
+      }
+  }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
